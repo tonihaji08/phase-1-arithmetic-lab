ecce170dcc06f1d3464630b000a17143eef8bc71
Your Name <antony.adriano@stundent.moringaschool.com>
2022-11-26 23:07:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+const num3 = 10;
+const num4 = 6;
+let mod = num3 % num4;
+let max = (2, 3, 10, 14, 20);
